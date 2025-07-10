81aeecb8aa85706a0ce4c1f8c3df626f793f535f
Raimund Andree <r.andree@live.com>
2025-07-10 23:13:54 +0200

    Add prefix.ps1 script for initializing AutomatedLab.Common module

---
+++ source/prefix.ps1
@@ -0,0 +1,35 @@
+$moduleBase = $PSScriptRoot
+$typeExists = try
+{
+    [AutomatedLab.Common.Win32Exception]
+}
+catch
+{
+}
+if (-not $typeExists)
+{
+    try
+    {
+        if ($PSEdition -eq 'Core')
+        {
+            Add-Type -Path $moduleBase/lib/core/AutomatedLab.Common.dll -ErrorAction Stop
+        }
+        else
+        {
+            Add-Type -Path $moduleBase/lib/full/AutomatedLab.Common.dll -ErrorAction Stop
+        }
+    }
+    catch
+    {
+        Write-Warning -Message "Unable to add AutomatedLab.Common.dll - GPO and PKI functionality might be impaired.`r`nException was: $($_.Exception.Message), $($_.Exception.LoaderExceptions)"
+    }
+}
+
+try
+{
+    [ServerCertificateValidationCallback]::Ignore()
+}
+catch
+{
+
+}
